2231a14cb667dff4a66e6639058bdc562e03d873
Sterling <sterling@Sterlings-MacBook-Pro.local>
2025-12-21 13:09:39 +0700

    Remove .next from git tracking and add to gitignore

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Rvgla6I8/Sa/FhioOLzFUQovOrjdTSgflVm3jrng01U=\"\n}"
--- .next/dev/static/chunks/app_page_tsx_9cca6c71._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/components_ui_map_index_tsx_ec365324._.js",
-  "static/chunks/_e36f87af._.js",
-  "static/chunks/node_modules_ec5234d7._.js"
-],
-    source: "dynamic"
-});
